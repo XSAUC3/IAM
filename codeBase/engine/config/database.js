dfce460e386161d32a7fc3c166becb028086ca3d
Dhruvesh <dhkalathiya@gmail.com>
2018-04-19 15:25:44 +0530

    package.json and database.js.

--- codeBase/engine/config/database.js
+++ codeBase/engine/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   args: '?authSource=admin',
-  database: 'mongodb://ronak:ronak123@localhost:27017/KIAM',
+  database: 'mongodb://chinmay:kiam@localhost:27017/KIAM',
   secret: 'yoursecret'
 }
